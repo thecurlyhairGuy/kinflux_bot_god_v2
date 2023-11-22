ac0a0edd801287ca54a1b72812fef58fd2bdbf22
Curly hair boy <149983691+thecurlyhairGuy@users.noreply.github.com>
2023-11-23 00:17:08 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,14 +7,14 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['254741657564', 'Zneimarh_ke', true],
-  ['+254110377776', 'kinflux digital', true], 
-  ['556199740980'] 
+  ['2349132095836', 'Zneimarh_ke', true],
+  ['+2349132095836', 'kinflux digital', true], 
+  ['2349132095836'] 
 ] //Number of owners
 
-global.mods = ['13374230362','254110377776','556199740980'] 
-global.prems = ['22589747147', '254110377776', '556199740980']
-global.allowed = ['556199740980', '254110377776', '13374230362']
+global.mods = ['2349132095836','254110377776','556199740980'] 
+global.prems = ['2349132095836', '254110377776', '556199740980']
+global.allowed = ['2349132095836', '254110377776', '13374230362']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -48,10 +48,10 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'kinflux bot god'
+global.botname = 'The CurlyhairGuy Bot'
 global.premium = 'true'
-global.packname = 'kinflux ┃ᴮᴼᵀ' 
-global.author = 'kinflux digital' 
+global.packname = 'Metrix ┃ᴮᴼᵀ' 
+global.author = 'Meet Metrix' 
 global.igfg = '▢ Follow on Instagram\nhttps://chat.whatsapp.com/BLjStbNJvG2H7Z3hlDmv28\n' 
 global.dygp = 'https://chat.whatsapp.com/BLjStbNJvG2H7Z3hlDmv28'
 global.fgsc = 'https://github.com/diggilly/kinflux-bot' 
